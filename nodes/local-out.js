e696b4b045b3d76c92c0e140d12af69b1d6265e2
Andrey Popov <andrey_popov@me.com>
2021-10-23 18:36:06 +0300

    TTS improvement

--- nodes/local-out.js
+++ nodes/local-out.js
@@ -2,7 +2,9 @@ module.exports = function(RED) {
     function AliceLocalOutNode(config) {
         RED.nodes.createNode(this,config);
         let node = this;
+        node.config = config;
         node.controller = RED.nodes.getNode(config.token);
+
         node.input = config.input;
         node.station = config.station_id;
         node.debugFlag = config.debugFlag;
@@ -11,6 +13,7 @@ module.exports = function(RED) {
         node.stopListening = config.stopListening;
         node.noTrackPhrase = config.noTrack;
         node.pauseMusic = config.pauseMusic;
+        node.ttsVoice = config.ttsVoice;
         node.status({});
 
         function debugMessage(text){
@@ -19,15 +22,62 @@ module.exports = function(RED) {
             }
         }
         debugMessage(node.station);
-        node.on('input', (data) => {
-            debugMessage(`input: ${JSON.stringify(data)}`)
-            if (node.volumeFlag) {data.volume = node.volume}
-            if (node.stopListening) {data.stopListening = node.stopListening}
-            if (node.noTrackPhrase) {data.noTrackPhrase = node.noTrackPhrase}
-            if (node.pauseMusic) {data.pauseMusic = node.pauseMusic}
+        node.on('input', (input) => {
+            debugMessage(`input: ${JSON.stringify(input)}`)
+
             if (node.station) {
+                var data = {};
+
+                //apply node's config
+                if (node.volumeFlag) {data.volume = node.volume/100}
+                if (node.stopListening) {data.stopListening = node.stopListening}
+                if (node.noTrackPhrase) {data.noTrackPhrase = node.noTrackPhrase}
+                if (node.pauseMusic) {data.pauseMusic = node.pauseMusic}
+
+
+                //redefine options from input
+                if ("volume" in input) {data.volume = input.volume/100}
+                if ("voice" in input) {node.ttsVoice = input.voice}
+                if ("prevent_listening" in input) {node.noTrackPhrase = input.prevent_listening}
+                if ("pause_music" in input) {data.pauseMusic = input.pause_music}
+
+                let payload;
+                switch (node.config.payloadType) {
+                    case 'flow':
+                    case 'global': {
+                        RED.util.evaluateNodeProperty(node.config.payload, node.config.payloadType, this, message, function (error, result) {
+                            if (error) {
+                                node.error(error, message);
+                            } else {
+                                payload = result;
+                            }
+                        });
+                        break;
+                    }
+                    case 'str': {
+                        payload = node.config.payload;
+                        break;
+                    }
+                    case 'json': {
+                        var arr = JSON.parse(node.config.payload);
+                        payload = arr[(Math.random() * arr.length) | 0];
+                        break;
+                    }
+                    case 'msg':
+                    default: {
+                        payload = input[node.config.payload];
+                        break;
+                    }
+                }
+                data.payload = payload;
+
+                if (node.ttsVoice) {
+                    data.payload = "<speaker voice='"+node.voice+"'>" + data.payload;
+                }
                 node.controller.sendMessage(node.station, node.input, data);
                 debugMessage(`Sending data: station: ${node.station}, input type: ${node.input}, data: ${JSON.stringify(data)}`);
+            } else {
+                debugMessage('node.station is empty');
             }
         });
 
